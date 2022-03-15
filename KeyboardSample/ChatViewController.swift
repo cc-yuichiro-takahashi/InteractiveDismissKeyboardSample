04a35cc7df503d3111f762b96b737824081b84fa
cc-yuichiro-takahashi <yuichiro.takahashi@coincheck.com>
2022-03-15 17:31:31 +0900

    Feature: imple interactive dismiss keyboard

--- KeyboardSample/ChatViewController.swift
+++ KeyboardSample/ChatViewController.swift
@@ -12,6 +12,7 @@ class ChatViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var textView: UITextView!
     @IBOutlet private weak var containerViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var panGestureRecognizer: UIPanGestureRecognizer!
 
     private let dummyMessages = [
         Message(text: "test", userName: "testUser", postDate: "2022/12/23")
@@ -22,16 +23,32 @@ class ChatViewController: UIViewController {
         return (scenes.first as? UIWindowScene)?.windows.first?.safeAreaInsets.bottom ?? 0
     }
 
+    private var currentKeyboardHeight: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureTableView()
+        configurePanGesture()
         observeKeyboardNotification()
     }
 
     @IBAction private func postButtonTapAction(_ sender: UIButton) {
         textView.endEditing(true)
         textView.text = ""
+    }
+
+    @IBAction private func panGestureAction(_ gestureRecognizer: UIPanGestureRecognizer) {
+        if case .changed = gestureRecognizer.state {
+            let origin = gestureRecognizer.location(in: view)
+            // 現在の指の位置
+            let height = UIScreen.main.bounds.height - origin.y
+
+            // 指がキーボードの高さより下に来たら入力フィールドも移動させる
+            if height <= self.currentKeyboardHeight {
+                self.containerViewBottomConstraint.constant = -(height - self.bottomSareAreaHeight)
+            }
+        }
     }
 }
 
@@ -44,6 +61,12 @@ private extension ChatViewController {
         tableView.transform = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: 0)
 
         tableView.dataSource = self
+
+        tableView.keyboardDismissMode = .interactive
+    }
+
+    func configurePanGesture() {
+        panGestureRecognizer.delegate = self
     }
 
     func observeKeyboardNotification() {
@@ -70,6 +93,7 @@ private extension ChatViewController {
         let keyboardHeight = keyboardFrame.size.height
 
         containerViewBottomConstraint.constant = -(keyboardHeight - bottomSareAreaHeight)
+        currentKeyboardHeight = keyboardHeight
 
         UIView.animate(withDuration: duration, animations: {
             self.view.layoutIfNeeded()
@@ -78,6 +102,7 @@ private extension ChatViewController {
 
     func keyboardWillHide(duration: Double) {
         containerViewBottomConstraint.constant = 0
+        currentKeyboardHeight = 0
 
         UIView.animate(withDuration: duration, animations: {
             self.view.layoutIfNeeded()
@@ -101,5 +126,11 @@ extension ChatViewController: UITableViewDataSource {
         cell.transform = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: 0)
 
         return cell
+    }
+}
+
+extension ChatViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        panGestureRecognizer == gestureRecognizer
     }
 }
